7361f1cc5af494dcdcb7d05c5562d93a2a1802e7
T. Burch <TBurchgart@hhek.de>
2022-11-08 17:16:39 +0100

    Added blitting

--- CLIEngine/CLIEngine.MarioBros/Canvas.cs
+++ CLIEngine/CLIEngine.MarioBros/Canvas.cs
@@ -13,9 +13,28 @@ public class Canvas
     }
 
     public void Blit(List<List<Rich>> layer, (int, int) locator) {
-        List<Rich> hMargin = new List<Rich>();
+        List<List<Rich>> result = new List<List<Rich>>();
         Rich vSpacer = new Rich(" ", Color.Opacity, BGColor.Opacity);
+        List<Rich> hFinalSpacer = Enumerable.Repeat(vSpacer, size.Item1).ToList();
 
+        Console.WriteLine(layer.ToString());
+
+        foreach(List<Rich> row in layer) {
+            List<Rich> hSpacer = Enumerable.Repeat(vSpacer, locator.Item1).ToList();
+            hSpacer.AddRange(row);
+            hSpacer.AddRange(Enumerable.Repeat(vSpacer, size.Item1-(row.Count+locator.Item1)));
+            result.Append(hSpacer);
+        }
+
+        for(int a = 0; a < locator.Item2; a++) {
+            result.Prepend(hFinalSpacer);
+        }
+
+        for(int b = 0; b < size.Item2-result.Count; b++) {
+            result.Append(hFinalSpacer);
+        }        
+
+        Console.WriteLine(layer.ToString());
     }
 
     public List<List<List<Rich>>> Layers() {
